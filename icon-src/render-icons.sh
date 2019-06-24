b2b7d6a78e9abaf03d944e616fac53a3835ecad4
Mirian Margiani <mirian@margiani.ch>
2019-06-24 22:49:41 +0200

    Make new icon

---
+++ icon-src/render-icons.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+app="harbour-wunderfitz"
+
+for i in 86 108 128 172 256; do
+    mkdir -p "../icons/${i}x$i"
+    inkscape -z -e "../icons/${i}x$i/$app.png" -w "$i" -h "$i" "$app.svg"
+done
+
+mkdir -p "../images"
+inkscape -z -e "../images/${app#harbour-}.png" -w "860" -h "860" "$app.svg"
+inkscape -z -e "../images/background.png" -w "860" -h "860" "$app.svg"
