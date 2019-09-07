b285b05f29794dee234b8ef9384490e8a4437981
Quentí <33203663+Quenty31@users.noreply.github.com>
2019-09-07 19:13:17 +0200

    Create harbour-dictionary-fr.ts

---
+++ translations/harbour-dictionary-fr.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Dictionary</source>
+        <translation>À propos de Dictionary</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Crédits</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv2</source>
+        <translation>Sous licence GNU GPLv2</translation>
+    </message>
+    <message>
+        <source>Open heinzelnisse.info</source>
+        <translation>Ouvrir heinzelnisse.info</translation>
+    </message>
+    <message>
+        <source>Open QuaZIP home page</source>
+        <translation>Aller à la page d'accueil de QuaZIP</translation>
+    </message>
+    <message>
+        <source>Open dict.cc home page</source>
+        <translation>Aller à la page d'accueil de dict.cc</translation>
+    </message>
+    <message>
+        <source>This application supports dict.cc dictionary export files. Thanks to Paul Hemetsberger and the dict.cc team for providing the dictionaries!</source>
+        <translation>This application supports dict.cc dictionary export files. Thanks to Paul Hemetsberger and the dict.cc team for providing the dictionaries!</translation>
+    </message>
+    <message>
+        <source>This application uses the QuaZIP library, version 0.7.2, to extract ZIP archives. Thanks to Sergey A. Tachenov and the QuaZIP contributors for making it so simple to extract ZIP files with Qt!</source>
+        <translation>This application uses the QuaZIP library, version 0.7.2, to extract ZIP archives. Thanks to Sergey A. Tachenov and the QuaZIP contributors for making it so simple to extract ZIP files with Qt!</translation>
+    </message>
+    <message>
+        <source>A mobile dictionary application for offline use including the Heinzelnisse database and supporting dict.cc export files.</source>
+        <translation>A mobile dictionary application for offline use including the Heinzelnisse database and supporting dict.cc export files.</translation>
+    </message>
+    <message>
+        <source>This project uses the Norwegian-German dictionary from heinzelnisse.info - Thanks to the authors Heiko Klein and Julia Emmerich for making the dictionary available under the conditions of the GNU GPLv2!</source>
+        <translation>This project uses the Norwegian-German dictionary from heinzelnisse.info - Thanks to the authors Heiko Klein and Julia Emmerich for making the dictionary available under the conditions of the GNU GPLv2!</translation>
+    </message>
+    <message>
+        <source>Dictionary was translated to several languages. Thanks to the contributors for making that happen!</source>
+        <translation>Dictionary was translated to several languages. Thanks to the contributors for making that happen!</translation>
+    </message>
+    <message>
+        <source>This is a fork of Wunderfitz without any online functionality.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forked sources on GitHub</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Original sources on GitHub</source>
+        <translation>Fichiers sources d'origine sur GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>DictionariesPage</name>
+    <message>
+        <source>Dictionaries</source>
+        <translation>Dictionnaires</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Download dict.cc dictionaries</source>
+        <translation>Télécharger des dictionnaires de dict.cc</translation>
+    </message>
+    <message>
+        <source>Import dict.cc ZIP archives</source>
+        <translation>Import dict.cc ZIP archives</translation>
+    </message>
+    <message>
+        <source>Dict.cc Import</source>
+        <translation>Dict.cc Import</translation>
+    </message>
+    <message>
+        <source>Dict.cc does not allow other applications such as Dictionary to ship their dictionaries. Therefore, you must download them from dict.cc yourself. Use the Download link, follow the instructions and import the files here afterwards. The downloaded dict.cc ZIP files must be placed in the Downloads folder. If in doubt, use the SailfishOS E-Mail and Browser apps to store the downloads there automatically. After the import in Dictionary you can delete the ZIP archives. Please note that you only need to download one combination of two languages. For example if you use DE-EN, you don&apos;t need EN-DE as Dictionary always searches in both languages.</source>
+        <translation>Dict.cc does not allow other applications such as Dictionary to ship their dictionaries. Therefore, you must download them from dict.cc yourself. Use the Download link, follow the instructions and import the files here afterwards. The downloaded dict.cc ZIP files must be placed in the Downloads folder. If in doubt, use the SailfishOS E-Mail and Browser apps to store the downloads there automatically. After the import in Dictionary you can delete the ZIP archives. Please note that you only need to download one combination of two languages. For example if you use for example DE-EN, you don&apos;t need EN-DE as Dictionary always searches in both languages.</translation>
+    </message>
+    <message>
+        <source>Dictionary</source>
+        <translation>Dictionnaire</translation>
+    </message>
+    <message>
+        <source>Choose the active dictionary here</source>
+        <translation>Choose the active dictionary here</translation>
+    </message>
+    <message>
+        <source>Delete selected dictionary</source>
+        <translation>Delete selected dictionary</translation>
+    </message>
+    <message>
+        <source>Dictionary %1 successfully imported</source>
+        <translation>Dictionary %1 successfully imported</translation>
+    </message>
+    <message>
+        <source>Deleting dictionary %1</source>
+        <translation>Deleting dictionary %1</translation>
+    </message>
+    <message>
+        <source>Dictionary %1 imported</source>
+        <translation>Dictionary %1 imported</translation>
+    </message>
+</context>
+<context>
+    <name>DictionaryButton</name>
+    <message>
+        <source>Dictionaries</source>
+        <translation>Dictionaries</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation>Loading...</translation>
+    </message>
+</context>
+<context>
+    <name>TitlePage</name>
+    <message>
+        <source>About Dictionary</source>
+        <translation>À propos de Dictionary</translation>
+    </message>
+    <message>
+        <source>Dictionaries</source>
+        <translation>Dictionnaires</translation>
+    </message>
+    <message>
+        <source>Pull down the menu to import and change your dictionaries</source>
+        <translation>Pull down the menu to import and change your dictionaries</translation>
+    </message>
+    <message>
+        <source>Searching...</source>
+        <translation>SRecherche...</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>Aucun résultat</translation>
+    </message>
+    <message>
+        <source>Search in dictionary...</source>
+        <translation>Chercher dans le dictionnaire...</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Copier dans le presse-papiers</translation>
+    </message>
+    <message>
+        <source>Search for &apos;%1&apos;</source>
+        <translation>Rechercher &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>Active Dictionary:</source>
+        <translation>Dictionnaire actif :</translation>
+    </message>
+</context>
+</TS>
